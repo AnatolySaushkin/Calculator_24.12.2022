1f021e9568a79002fd5f0d82f6bf0fcf9b8ccccd
Anatoly Saushkin <blond96@bk.ru>
2022-12-24 17:47:39 +0300

    Создание поля для калькулятора. Прописание кнопок

--- app/src/main/java/com/Saushkin/calculator_24122022/MainActivity.java
+++ app/src/main/java/com/Saushkin/calculator_24122022/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.Saushkin.calculator_24122022.R;
+
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
